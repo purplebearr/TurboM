a708fb3b08e899d2ab998e95167a5bf289dd0d3c
gigaorus <imaveryniceperson@protonmail.com>
2024-08-08 17:56:12 -0700

    V1.0

--- controllers.py
+++ controllers.py
@@ -1,17 +1,12 @@
-# controllers.py
 from template_engine import render_template
 
-def index(params):
-    context = {
-        'title': 'Home',
-        'message': 'Welcome to our MVC Framework!',
-        'items': ['Python', 'MVC', 'Web Development']
-    }
-    return render_template('index.html', context)
 
-def about(params):
-    context = {
-        'title': 'About',
-        'content': 'This is a simple about page.'
-    }
-    return render_template('about.html', context)
+#Demo controller view
+
+# def index(params):
+#     context = {
+#         'title': 'Home',
+#         'message': 'Welcome to our MVC Framework!',
+#         'items': ['Python', 'MVC', 'Web Development']
+#     }
+#     return render_template('index.html', context)
